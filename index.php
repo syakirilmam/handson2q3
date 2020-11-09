d873edfc7203ee2dd88557cb602b2801585e0e35
syakirilmam <70127870+syakirilmam@users.noreply.github.com>
2020-11-09 21:18:22 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php 
+   $my_string = 'Hai!! Guys, I am Muhammad Syakir Ilmam Bin Ariffin and Cloud Computer Expert! Pleasego to my GitHub account : https://github.com/syakirilmam '; 
+   echo $my_string; 
+?> 
